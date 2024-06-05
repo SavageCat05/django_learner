129077af92a4d556a52d83135fb2681a57281bd5
Anshuman Raj <savagecatwillcode@gmail.com>
2024-06-05 14:24:10 +0530

    learning about models

--- book_store/book_store/book_outlet/models.py
+++ book_store/book_store/book_outlet/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+
 class book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    best_selling = models.BooleanField(default = False )
 
     def __str__(self):
-        return f"title is: {self.title}, rating is {self.rating}"
+        return f"\ntitle is: {self.title},\n rating is: {self.rating},\n author is:{self.author}, \n Is it best-selling: {self.best_selling} "
