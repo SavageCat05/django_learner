cbb490ed8c91b7503a211d7e9e7f8c3dd0b42860
Anshuman Raj <savagecatwillcode@gmail.com>
2024-06-11 11:42:19 +0530

    part 2/4 completed

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Author(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=150)
+    date = models.DateField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null = True, related_name="posts")
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    tag = models.ManyToManyField(Tag)
+    image = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,90 +1,85 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Anshuman",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Anshuman",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Anshuman",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
-
-def get_date(list1):
-    return list1['date']
-# Adding a helper function, lambda 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key= get_date) #sorted function makes a new list
-    latest_posts = sorted_posts[-3:]
-    print("This will be printed")
+    latest_posts = Post.objects.all().order_by("-date")[0:3]
     return render(request, 'blog/starting_page.html', {'latest_posts':latest_posts})
 
 
 def post(request):
-    sorted_posts = sorted(all_posts, key= get_date) #sorted function makes a new list
-    latest_posts = sorted_posts[-3:]
-    print("This will be printed")
+    latest_posts = Post.objects.all().order_by("-date")
     return render(request, 'blog/all-posts.html', {'latest_posts':latest_posts})
 
 def selected_post(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug = slug)
     return render(request, 'blog/post-detail.html', {
         'post':identified_post
     })
+
+
+
+    # {
+    #     "slug": "hike-in-the-mountains",
+    #     "image": "mountains.jpg",
+    #     "author": "Anshuman",
+    #     "date": date(2021, 7, 21),
+    #     "title": "Mountain Hiking",
+    #     "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+    #     "content": """
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #     """
+    # },
+    # {
+    #     "slug": "programming-is-fun",
+    #     "image": "coding.jpg",
+    #     "author": "Anshuman",
+    #     "date": date(2022, 3, 10),
+    #     "title": "Programming Is Great!",
+    #     "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+    #     "content": """
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #     """
+    # },
+    # {
+    #     "slug": "into-the-woods",
+    #     "image": "woods.jpg",
+    #     "author": "Anshuman",
+    #     "date": date(2020, 8, 5),
+    #     "title": "Nature At Its Best",
+    #     "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+    #     "content": """
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+    #       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+    #       aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+    #       velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+    #     """
+    # }
